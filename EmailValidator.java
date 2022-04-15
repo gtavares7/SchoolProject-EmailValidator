5e275ea5b242bb66191e9eede3e9e0a2129c96f4
gtavares7 <gtavares.linux@gmail.com>
2022-04-15 11:50:13 -0400

    added methods to split the email before and after the @

--- EmailValidator.java
+++ EmailValidator.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class EmailValidator {
 
-    // check if characters in the email are alphanumeric
+    // check if the characters in the email are alphanumeric
     public boolean isAlphanumeric(char c) {
 
-        // check to see if a character is either a letter or digit
-        if (Character.isDigit(c)) || Character.isLetter(c)) {
+        // Check to see if a character is either a letter or a digit
+        if (Character.isDigit(c) || Character.isLetter(c)) {
             return true;
         } else {
             return false;
@@ -49,6 +49,23 @@ public class EmailValidator {
             }
         }
         return oneAt == 1;
+    }
+
+    // method to get email prefix
+    public static String getPrefix(String email) {
+
+        // split email at the "@"
+        String[] emailSplit = email.split("@");
+        // return the prefix before the "@"
+        return emailSplit[0];
+    }
+
+    // method to get email domain
+    public static String getDomain(String email) {
+
+        String[] emailSplit = email.split("@");
+        // return the domains after the "@"
+        return emailSplit[1];
     }
 
 
