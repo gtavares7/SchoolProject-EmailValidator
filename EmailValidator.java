ec1a95474dc493aed6e81d50fd2080fe7239c5a0
gtavares7 <gtavares.linux@gmail.com>
2022-04-15 11:33:06 -0400

    added boolean to validate email domain characters

--- EmailValidator.java
+++ EmailValidator.java
@@ -16,7 +16,7 @@ public class EmailValidator {
     // check if email characters are valid prefix characters
     public boolean isValidPrefixChar(char c) {
 
-        // check to see if a prefix character is alphanumeric or contains a dash, a period or an underscore
+        // check if prefix characters are alphanumeric or contains a dash, a period or an underscore
         if (isAlphanumeric(c) || c == '-' || c == '.' || c == '_') {
             return true;
         } else {
@@ -24,7 +24,16 @@ public class EmailValidator {
         }
     }
 
+    // check if the email domain contains only valid characters
+    public boolean isValidDomainChar(char c) {
 
+        //check if domain characters are alphanumeric or contains a dash or a period
+        if (isAlphanumeric(c) || c == '-' || c == '.') {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 
 }
