aec3ef65e711584d8fa2ff39f22321cbda7bcb59
gtavares7 <gtavares.linux@gmail.com>
2022-04-15 11:26:48 -0400

    added boolean checks for valid alphanumeric and prefix characters

--- EmailValidator.java
+++ EmailValidator.java
@@ -1,4 +1,30 @@
+import java.util.*;
+
 public class EmailValidator {
+
+    // check if characters in the email are alphanumeric
+    public boolean isAlphanumeric(char c) {
+
+        // check to see if a character is either a letter or digit
+        if (Character.isDigit(c)) || Character.isLetter(c)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // check if email characters are valid prefix characters
+    public boolean isValidPrefixChar(char c) {
+
+        // check to see if a prefix character is alphanumeric or contains a dash, a period or an underscore
+        if (isAlphanumeric(c) || c == '-' || c == '.' || c == '_') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 
 
 }
